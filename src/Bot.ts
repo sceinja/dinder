98863095da9528af286a2aa50af108b55541f807
Adil <86347560+sceinja@users.noreply.github.com>
2023-01-07 22:56:11 +0500

    removed token

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,7 +1,7 @@
 import { Client } from "discord.js";
 import ready from "./listeners/ready";
 
-const token = 'MTA1NjQxMjk5OTkzMTM5NjIwNw.Gp5h9r.KhrfCWqrVLR4YLdxBVF5hz_q49ERHmceywJY3w';
+const token = '';
 
 console.log("Dinder is starting . . .");
 
